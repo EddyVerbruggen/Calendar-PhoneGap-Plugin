750d4726dd7b9160014e1c13ca7a6ef8e1d33598
Christian Schmidt <c.schmidt@exec.de>
2023-10-05 11:08:48 +0200

    ios17 fix for calendar access

--- src/ios/Calendar.h
+++ src/ios/Calendar.h
@@ -5,43 +5,44 @@
 
 @interface Calendar : CDVPlugin <EKEventEditViewDelegate>
 
-@property (nonatomic, retain) EKEventStore* eventStore;
-@property (nonatomic, copy) NSString *interactiveCallbackId;
+@property(nonatomic, retain) EKEventStore *eventStore;
+@property(nonatomic, copy) NSString *interactiveCallbackId;
 
-- (void)initEventStoreWithCalendarCapabilities;
+- (void)initEventStoreWithFullCalendarCapabilities;
+- (void)initEventStoreWithWriteCalendarCapabilities;
 
--(NSArray*)findEKEventsWithTitle: (NSString *)title
-                        location: (NSString *)location
-                           notes: (NSString *)notes
-                       startDate: (NSDate *)startDate
-                         endDate: (NSDate *)endDate
-                       calendars: (NSArray *)calendars;
+- (NSArray *)findEKEventsWithTitle:(NSString *)title
+                          location:(NSString *)location
+                             notes:(NSString *)notes
+                         startDate:(NSDate *)startDate
+                           endDate:(NSDate *)endDate
+                         calendars:(NSArray *)calendars;
 
-- (void)hasReadPermission:(CDVInvokedUrlCommand*)command;
-- (void)requestReadPermission:(CDVInvokedUrlCommand*)command;
+- (void)hasReadPermission:(CDVInvokedUrlCommand *)command;
+- (void)requestReadPermission:(CDVInvokedUrlCommand *)command;
 
-- (void)hasWritePermission:(CDVInvokedUrlCommand*)command;
-- (void)requestWritePermission:(CDVInvokedUrlCommand*)command;
+- (void)hasWritePermission:(CDVInvokedUrlCommand *)command;
+- (void)requestWritePermission:(CDVInvokedUrlCommand *)command;
 
-- (void)hasReadWritePermission:(CDVInvokedUrlCommand*)command;
-- (void)requestReadWritePermission:(CDVInvokedUrlCommand*)command;
+- (void)hasReadWritePermission:(CDVInvokedUrlCommand *)command;
+- (void)requestReadWritePermission:(CDVInvokedUrlCommand *)command;
 
-- (void)openCalendar:(CDVInvokedUrlCommand*)command;
-- (void)createCalendar:(CDVInvokedUrlCommand*)command;
-- (void)deleteCalendar:(CDVInvokedUrlCommand*)command;
+- (void)openCalendar:(CDVInvokedUrlCommand *)command;
+- (void)createCalendar:(CDVInvokedUrlCommand *)command;
+- (void)deleteCalendar:(CDVInvokedUrlCommand *)command;
 
-- (void)createEventWithOptions:(CDVInvokedUrlCommand*)command;
-- (void)createEventInteractively:(CDVInvokedUrlCommand*)command;
-- (void)modifyEventWithOptions:(CDVInvokedUrlCommand*)command;
+- (void)createEventWithOptions:(CDVInvokedUrlCommand *)command;
+- (void)createEventInteractively:(CDVInvokedUrlCommand *)command;
+- (void)modifyEventWithOptions:(CDVInvokedUrlCommand *)command;
 
-- (void)findEventWithOptions:(CDVInvokedUrlCommand*)command;
-- (void)findAllEventsInNamedCalendar:(CDVInvokedUrlCommand*)command;
+- (void)findEventWithOptions:(CDVInvokedUrlCommand *)command;
+- (void)findAllEventsInNamedCalendar:(CDVInvokedUrlCommand *)command;
 
-- (void)listCalendars:(CDVInvokedUrlCommand*)command;
-- (void)deleteEvent:(CDVInvokedUrlCommand*)command;
-- (void)deleteEventFromNamedCalendar:(CDVInvokedUrlCommand*)command;
-- (void)deleteEventFromCalendar:(CDVInvokedUrlCommand*)command calendar:(EKCalendar*)calendar;
-- (void)deleteEventById:(CDVInvokedUrlCommand*)command;
-- (void)eventEditViewController:(EKEventEditViewController*)controller didCompleteWithAction:(EKEventEditViewAction) action;
+- (void)listCalendars:(CDVInvokedUrlCommand *)command;
+- (void)deleteEvent:(CDVInvokedUrlCommand *)command;
+- (void)deleteEventFromNamedCalendar:(CDVInvokedUrlCommand *)command;
+- (void)deleteEventFromCalendar:(CDVInvokedUrlCommand *)command calendar:(EKCalendar *)calendar;
+- (void)deleteEventById:(CDVInvokedUrlCommand *)command;
+- (void)eventEditViewController:(EKEventEditViewController *)controller didCompleteWithAction:(EKEventEditViewAction)action;
 
 @end
